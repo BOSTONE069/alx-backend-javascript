e60d7b2bf2153e5b2ee5a5f2f37b4692cc864d9c
BOSTONE069 <bostoneochieng69@gmail.com>
2023-01-23 16:18:48 -0500

    Usage of arrows in the program code

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+/**
+ * This function returns an object with two properties: an array of neighborhoods and a function that
+ * adds a new neighborhood to the array.
+ */
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
