0e4f18fa78d85b40f60d0dc2e41123154e6de7fe
BOSTONE069 <bostoneochieng69@gmail.com>
2023-01-23 16:17:33 -0500

    [200~modify the variables inside the function taskBlock so that the variables aren’t overwritten inside the conditional block.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,3 +1,8 @@
+/**
+ * If trueOrFalse is true, then return [true, false], otherwise return [false, true].
+ * @param trueOrFalse - a boolean value
+ * @returns [false, true]
+ */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
