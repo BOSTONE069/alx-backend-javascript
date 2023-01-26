effc5cfddf9e067d301791e6d046ccb9ea27d6eb
BOSTONE069 <bostoneochieng69@gmail.com>
2023-01-26 15:29:52 -0500

    Implement a class named Car

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
