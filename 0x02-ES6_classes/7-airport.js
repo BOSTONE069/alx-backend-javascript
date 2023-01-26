479da1cfafa9a9d59f34aeaed808a8159f55ce3c
BOSTONE069 <bostoneochieng69@gmail.com>
2023-01-26 15:18:42 -0500

    implementation of the airport class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,26 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  toString() {
+    return this._code;
+  }
+}
